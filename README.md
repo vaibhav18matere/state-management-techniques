9cfe0d47857f75291f2ffa6ef454c8438fc662a1
VaibhavMatere <59862355+vaibhav18matere@users.noreply.github.com>
2024-02-21 17:44:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React + Vite + Tailwind CSS Template
+
+- A sample TEMPLATE repository to get started any new app without much hustle and also don't need to install dependencies everytime
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+function App() {
+
+  return (
+    <>
+      <h1 className='text-center text-2xl'>React + Vite + Tailwind CSS</h1>
+      <p>A sample TEMPLATE project to get started any new app</p>
+    </>
+  )
+}
+
+export default App
